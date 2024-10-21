92afbe44eb433d6375dc49612087923d910218c0
Giselle-Evy <Gislyngonzalez077@gmail.com>
2024-10-21 09:24:46 -0600

    navbar general de las tablas

---
+++ src/Components/TablaProducto/NavbarComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><a href="">Inventario</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Ventas</a></li>
+                <li><a href="">Proveedores</a></li>
+                <li><a href="">Empleados</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
