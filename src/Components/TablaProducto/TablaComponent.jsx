75626779a5a738c83d69f67c16c477b703c788f5
Giselle-Evy <Gislyngonzalez077@gmail.com>
2024-10-21 09:34:54 -0600

    Estructura de la Tabla general

---
+++ src/Components/TablaProducto/TablaComponent.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const ProductTable = ({ products }) => {
+    return (
+        <table className="product-table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Precio</th>
+                    <th>Proveedor_id</th>
+                    <th>Categoría</th>
+                    <th>Estado</th>
+                </tr>
+            </thead>
+            <tbody>
+                {products.map((product) => (
+                    <tr key={product.id}>
+                        <td>{product.id}</td>
+                        <td>{product.name}</td>
+                        <td>{product.description}</td>
+                        <td>{product.price}</td>
+                        <td>{product.providerId}</td>
+                        <td>{product.category}</td>
+                        <td>{product.status}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
+
+export default ProductTable;
+
+
