699291bc00467dc2e9f1b8e7cee9a4d79322dbe9
Oldream31 <jenifer.gonzalez.nah.3e@gmail.com>
2024-10-29 21:33:14 -0600

    Conexion a la ruta de las tablas

--- src/Components/TablaProducto/NavbarComponent.jsx
+++ src/Components/TablaProducto/NavbarComponent.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+
 const Navbar = () => {
     return (
         <nav className="navbar2">
             <ul>
-                <li><a href="">Inventario</a></li>
-                <li><a href="">Productos</a></li>
-                <li><a href="">Ventas</a></li>
-                <li><a href="">Proveedores</a></li>
-                <li><a href="">Empleados</a></li>
+                <li><a href=""><Link to="/Inventario" replace={true}>Inventario</Link></a></li>
+                <li><a href=""><Link to="/Producto" replace={true}>Productos</Link></a></li>
+                <li><a href=""><Link to="/Ventas" replace={true}>Ventas</Link></a></li>
+                <li><a href=""><Link to="/Proveedores" replace={true}>Prooveedores</Link></a></li>
+                <li><a href=""><Link to="/Empleado" replace={true}>Empleados</Link></a></li>
             </ul>
         </nav>
     );
